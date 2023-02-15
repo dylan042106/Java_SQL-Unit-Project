5e0f3fa4db924a8ba1f0d993b2c399b7871f44db
Dylan <dylan04herring@gmail.com>
2023-02-15 13:52:43 -0600

    Added Connection Method

--- src/main/java/org/JavaSQLUnitProject/Main.java
+++ src/main/java/org/JavaSQLUnitProject/Main.java
@@ -1,7 +1,25 @@
 package org.JavaSQLUnitProject;
+import java.sql.*;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        ConnectionToDB();
+        System.out.println("Welcome to St. Bonaventure Hospital");
+        System.out.println("Would you like to login as a patient or nurse?");
+    }
+
+    public static void ConnectionToDB() {
+        String myDriver = "org.postgresql.Driver";
+        String dbURL = "jdbc:postgresql://localhost:5432/hospital_registration";
+        String dbUser = "dylan_herring";
+        String dbPass = "062104";
+
+        try{
+            Class.forName(myDriver);
+            Connection conn = DriverManager.getConnection(dbURL, dbUser, dbPass);
+            System.out.println("Connection Established");
+        } catch (Exception e){
+            System.out.println(e);
+        }
     }
 }
